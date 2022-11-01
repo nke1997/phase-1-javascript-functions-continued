e2c72250a95a3046c60b81d22e3b9b061be7a2f9
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-01 15:06:38 -0400

    commit tree 24db2227cf0b7c2129060ee81ba32cfa1fa4b57f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(workplace = "go to the office") {
+    return `This Monday, I will ${workplace}.`;
+};
+
+function wrapAdjective(symbol = "*") {
+    return function (descriptor = "a special programmer") {
+        return `You are ${symbol}${descriptor}${symbol}!`};
+    };
